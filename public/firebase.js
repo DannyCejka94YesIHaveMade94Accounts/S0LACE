47836034d58b8af724c40eee65492ab4992a1753
grant <grantschmidt8@gmail.com>
2025-12-09 18:55:09 -0600

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,4 +1,5 @@
-<script type="module">
+// firebase.js (MODULE FILE)
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
@@ -16,4 +17,3 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-</script>

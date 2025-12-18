e5d2e80bbb7ab43c4ebd53d6e569645748a84be8
grant <grantschmidt8@gmail.com>
2025-12-17 22:41:43 -0600

    Update firebase-init.js

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,19 +1,14 @@
-// firebase.js (MODULE FILE)
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDZ9RpsmhIb6wMXCmysScNqfGY4K0Y3GvQ",
-  authDomain: "s0lace.firebaseapp.com",
-  projectId: "s0lace",
-  storageBucket: "s0lace.firebasestorage.app",
-  messagingSenderId: "987787551956",
-  appId: "1:987787551956:web:ffc45e1cc11b0bee34c741",
-  measurementId: "G-W79F0ZGEPN"
+const firebaseConfig = {
+  apiKey: "AIzaSyBbgy76ThxHaKlOhpiLQ6Nw4ekR1b9t3DA",
+  authDomain: "s0lace-web.firebaseapp.com",
+  databaseURL: "https://s0lace-web-default-rtdb.firebaseio.com",
+  projectId: "s0lace-web"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const db = getDatabase(app);
